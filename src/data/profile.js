b5dd289fb55bbe80f31cd9d259b8ef72a887b6a3
MadhuBasava4832 <madhubasava32@gmail.com>
2025-02-25 11:54:21 +0530

    My portfolio second commit

--- src/data/profile.js
+++ src/data/profile.js
@@ -2,13 +2,12 @@ const ProfileData = {
   img: require("../assets/Images/22A91A04D0.png"),
   logo: require("../assets/Images/madhulogo.png"),
   name: "Madhu Basava",
-  professions: ["Web Developer", "Trainee"],
+  professions: ["Web Developer", "Open to work"],
   info: [
-    "Knack of building web applications using MERN stack.",
-    "I enjoy designing tech websites.",
+    "Passionate about crafting seamless user experiences through full-stack development.",
+    "Building dynamic and efficient web solutions with expertise in both front-end and back-end technologies."
   ],
-  resume:
-    "",
+  resume: "./22A91A04D0_CV.pdf",
 };
 
 export default ProfileData;
